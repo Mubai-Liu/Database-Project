85eb9b04889636fe3b06a24123e3243eae9072d5
Mubai Liu <liumubai000@126.com>
2020-04-12 17:14:01 -0400

    update style

--- server.py
+++ server.py
@@ -165,7 +165,7 @@ def home():
 #
 @app.route('/another')
 def another():
-  return render_template("another.html")
+  return render_template("register.html")
 
 # @app.route('/companyInfo')
 # def companyListing():
@@ -247,7 +247,21 @@ def add():
   return redirect('/')
 
 
-
+@app.route('/pythonlogin/register', methods=['GET', 'POST'])
+def register():
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username", "password" and "email" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+    elif request.method == 'POST':
+        # Form is empty... (no POST data)
+        msg = 'Please fill out the form!'
+    # Show registration form with message (if any)
+    return render_template('register.html', msg=msg)
 
 
 
