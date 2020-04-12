6a1a966343f9287250096f4ac78aca3fb0abbc2e
Mubai Liu <liumubai000@126.com>
2020-04-11 21:42:39 -0400

    testing

--- server.py
+++ server.py
@@ -107,7 +107,7 @@ def index():
   #
   # example of a database query
   #
-  cursor = g.conn.execute("SELECT name FROM test")
+  cursor = g.conn.execute("SELECT * FROM person;")
   names = []
   for result in cursor:
     names.append(result['name'])  # can also be accessed using result[0]
